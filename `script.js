e53a75473068b2ba74857b5456eba062a2b1287f
Nicolò-Graziani-2C-JCmaxwell-2023 <101812261+Nicolo-Graziani-2C-JCmaxwell-2023@users.noreply.github.com>
2023-04-18 14:57:08 +0200

    Add files via upload

---
+++ `script.js
@@ -0,0 +1,41 @@
+// settaggio delle carte
+card = ["front4.jpg", "front.jpg", "front3.jpg", "front.jpg", "front2.jpg", "front3.jpg", "front2.jpg", "front4.jpg"]
+// set valore delle carte girate
+let NTurnCard = 0;
+let FristCard = null
+table = document.querySelector('#gametable')
+let ctht = document.getElementById("ctht");
+// set valore del 'conto' 
+let ct = 0;
+//setting delle varie
+card.forEach(element => {
+    table.innerHTML += '<img id="' + element + '" src="back.jpg" onclick="gira(this)" />'
+    console.log('<img id="' + element + '" back.jpg" />')
+});
+// settaggio della funzione
+function gira(card) {
+    // aggiunta delle carte girate
+    NTurnCard++
+    console.log(NTurnCard)
+    card.src = card.id
+    if (NTurnCard == 1)
+    FristCard = card
+    else {
+        NTurnCard = 0
+        if (card.src == FristCard.src) {
+            // aggiunta del punto
+            console.log("+1 Point")
+            ct++;
+            ctht.textContent = ct;
+        } else {
+            // avviso per l'errore
+            console.log("retry")
+            // settaggio del timer
+            setTimeout(function() {
+                card.src = "back.jpg";
+                FristCard.src = "back.jpg";
+            }, 1500);
+        }
+    }
+    ctht.textContent = ct;
+}
